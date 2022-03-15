80e3a0d1e4bc22b5be847f334f14334b507498a2
Joel García <joel.garcia.wk@gmail.com>
2022-03-15 18:17:56 +0100

    Cambiadas las funciones (aun no completas)

--- js/listaVerbos.js
+++ js/listaVerbos.js
@@ -146,24 +146,16 @@ var verbos = [
 ["write","wrote","written","escribir"]
 ];
 
+
 function sacarVerbo1(){            
     var i = 0;
-    var numeroRandom2 = Math.floor((Math.random() * (2 - 0 + 1)) + 0);
-    var verbo1 = verbos[i][numeroRandom2].toString();
+    var verbo1 = verbos[i][0].toString();
     return verbo1;
-    if(palabraIntroducida = verbos[i][3].toString()){
-        console.log("suma 1 al primer verbo");
-        i++;
-    }
 }
 
 function sacarVerbo2(){
     var i=0;
-    var numeroRandom3 = Math.floor((Math.random() * (2 - 0 + 1)) + 0);
+    var numeroRandom3 = Math.floor((Math.random() * (2 - 1)) + 1);
     var verbo2 = verbos[i][numeroRandom3].toString();
     return verbo2;
-    if(palabraIntroducida = verbos[i][3].toString()){
-        console.log("suma 1 al segundo verbo");
-        i++;
-    }
 }
